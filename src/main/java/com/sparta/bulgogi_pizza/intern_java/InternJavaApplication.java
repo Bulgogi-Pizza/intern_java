42a97331c685960276857e94ba22bdb2b5121436
Bulgogi-Pizza <quitendexit@gmail.com>
2025-06-12 00:19:37 +0900

    init commit

---
+++ src/main/java/com/sparta/bulgogi_pizza/intern_java/InternJavaApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.bulgogi_pizza.intern_java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InternJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InternJavaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sparta/bulgogi_pizza/intern_java/InternJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.bulgogi_pizza.intern_java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InternJavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
